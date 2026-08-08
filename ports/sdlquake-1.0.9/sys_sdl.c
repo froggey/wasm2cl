fd61630e91a8c039abc8fd3ad902366e16950202
Sylvia Harrington <sylvia.harrington28@gmail.com>
2026-08-08 14:04:16 +0100

    sdlquake: Import Iota fixes
    1. No networking
    2. No assembly
    3. No cdrom

--- ports/sdlquake-1.0.9/sys_sdl.c
+++ ports/sdlquake-1.0.9/sys_sdl.c
@@ -12,11 +12,13 @@
 #include <ctype.h>
 #include <errno.h>
 #ifndef __WIN32__
+#ifndef __wasm__
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <sys/mman.h>
+#endif
 #endif
 
 #include "quakedef.h"
